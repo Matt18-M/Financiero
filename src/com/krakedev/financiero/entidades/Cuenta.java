6db8c69b1aa3448396f5e849b150b011d4364902
Mateo Molina <robert.mateo.3012@gmail.com>
2026-04-23 00:28:38 -0500

    feat: implementar clase Banco, añadiendo método: depositar, retirar, transferir

--- src/com/krakedev/financiero/entidades/Cuenta.java
+++ src/com/krakedev/financiero/entidades/Cuenta.java
@@ -2,56 +2,53 @@ package com.krakedev.financiero.entidades;
 
 public class Cuenta {
 
-    private String id;
-    private double saldoActual;
-    private String tipo;
-    private Cliente propietario;
+	private String id;
+	private double saldoActual;
+	private String tipo;
+	private Cliente propietario;
 
-    public Cuenta(String id) {
-        this.id = id;
-        this.saldoActual = 0;
-        this.tipo = "A";
-        this.propietario = new Cliente();
-    }
+	public Cuenta(String id) {
+		this.id = id;
+		this.saldoActual = 0;
+		this.tipo = "A";
+		this.propietario = new Cliente();
+	}
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public double getSaldoActual() {
-        return saldoActual;
-    }
+	public double getSaldoActual() {
+		return saldoActual;
+	}
 
-    public void setSaldoActual(double saldoActual) {
-        this.saldoActual = saldoActual;
-    }
+	public void setSaldoActual(double saldoActual) {
+		this.saldoActual = saldoActual;
+	}
 
-    public String getTipo() {
-        return tipo;
-    }
+	public String getTipo() {
+		return tipo;
+	}
 
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 
-    public Cliente getPropietario() {
-        return propietario;
-    }
+	public Cliente getPropietario() {
+		return propietario;
+	}
 
-    public void setPropietario(Cliente propietario) {
-        this.propietario = propietario;
-    }
+	public void setPropietario(Cliente propietario) {
+		this.propietario = propietario;
+	}
 
-    public void imprimir() {
-        String mensaje;
+	public void imprimir() {
+		String mensaje;
 
-        mensaje = "ID: " + id +
-                  " , Saldo: " + saldoActual +
-                  " , Tipo: " + tipo +
-                  " , Cédula cliente: " + propietario.getCedula() +
-                  " , Nombre cliente: " + propietario.getNombre() +
-                  " , Apellido cliente: " + propietario.getApellido();
+		mensaje = "ID: " + id + " , Saldo: " + saldoActual + " , Tipo: " + tipo + " , Cédula cliente: "
+				+ propietario.getCedula() + " , Nombre cliente: " + propietario.getNombre() + " , Apellido cliente: "
+				+ propietario.getApellido();
 
-        System.out.println(mensaje);
-    }
+		System.out.println(mensaje);
+	}
 }
---
+++ src/com/krakedev/financiero/servicios/Banco.java
@@ -0,0 +1,56 @@
+package com.krakedev.financiero.servicios;
+
+import com.krakedev.financiero.entidades.Cliente;
+import com.krakedev.financiero.entidades.Cuenta;
+
+public class Banco {
+
+    private int ultimoCodigo = 1000;
+
+    public Banco() {
+    }
+
+    public int getUltimoCodigo() {
+        return ultimoCodigo;
+    }
+
+    public void setUltimoCodigo(int ultimoCodigo) {
+        this.ultimoCodigo = ultimoCodigo;
+    }
+
+    public Cuenta crearCuenta(Cliente cliente) {
+        String codigoStr = ultimoCodigo + "";
+        ultimoCodigo++;
+
+        Cuenta cuenta = new Cuenta(codigoStr);
+        cuenta.setPropietario(cliente);
+
+        return cuenta;
+    }
+
+    public boolean depositar(double monto, Cuenta cuenta) {
+        if (monto > 0) {
+            cuenta.setSaldoActual(cuenta.getSaldoActual() + monto);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean retirar(double monto, Cuenta cuenta) {
+        if (monto > 0 && monto <= cuenta.getSaldoActual()) {
+            cuenta.setSaldoActual(cuenta.getSaldoActual() - monto);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transferir(Cuenta origen, Cuenta destino, double monto) {
+        boolean retiro = retirar(monto, origen);
+
+        if (retiro) {
+            depositar(monto, destino);
+            return true;
+        }
+        return false;
+    }
+}
